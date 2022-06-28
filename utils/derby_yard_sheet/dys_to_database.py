18a3d32eae002ecf2f13698ca02733cd7dc4b482
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-06-28 11:16:55 +0100

    Changed MYSQL Database details to env variables

--- utils/derby_yard_sheet/dys_to_database.py
+++ utils/derby_yard_sheet/dys_to_database.py
@@ -1,13 +1,17 @@
+from dotenv import load_dotenv
+import os
 import derby_yard_sheet as dys
 from sqlalchemy import create_engine
 
+load_dotenv()
+
 data = dys.derby_yard_sheet()
 
-database_username = 'sam'
-database_password = 'garden79'
-database_ip = '139.59.171.54'
-database_name = 'derby_yard_sheets'
+database_username = os.getenv("MYSQLUSERNAME")
+database_password = os.getenv("MYSQLPASSWORD")
+database_ip = os.getenv("MYSQLIP")
+
 database_connection = create_engine(
-    f"mysql://{database_username}:{database_password}@{database_ip}/{database_name}")
+    f"mysql://{database_username}:{database_password}@{database_ip}/'derby_yard_sheets'")
 
 data.to_sql('yard_sheets', con=database_connection, if_exists='append')

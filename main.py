b467211a13f6cdc3f5b891581eb006e5e0d141fb
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-07-21 16:42:22 +0100

    Continued to work on the spend split

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.asset_file.asset_file import merged_query as mq
 import pandas as pd
-from utils.functions.vehicle_spend import all_spend
+from utils.functions.vehicle_spend import all_fleet_split
 
-dataframe = all_spend
+dataframe = all_fleet_split()
 dataframe.to_excel('output.xlsx', index=False)
 
 
--- utils/functions/vehicle_spend.py
+++ utils/functions/vehicle_spend.py
@@ -7,16 +7,41 @@ single_vehicle_query = str(queries.vehicle_spend_query(vehicle=1406, from_date="
 vehicle_spend = pd.read_sql(single_vehicle_query, cnxn)
 
 
-def spend_splitter(vehicle, table):
+def all_fleet_split():
+    all_fleet_split = str(queries.all_spend_split())
+    all_spend = pd.read_sql(all_fleet_split, cnxn)
+    all_spend['Months Between'] = all_spend.apply(calculate_months, axis=1)
+
+    spend_split = calculate_all_spend(all_spend)
+
+    all_spend['3'] = all_spend.apply(calculate_three_month_spend, args=(spend_split,), axis=1)
+    return all_spend
+
+
+def calculate_months(vehicle):
     job_date = vehicle['job_date']
     today = date.today()
     months_between = (today.year - job_date.year) * 12 + today.month - job_date.month
     return months_between
 
 
-all_fleet_split = str(queries.all_spend_split())
-all_spend = pd.read_sql(all_fleet_split, cnxn)
-all_spend['Months Between'] = all_spend.apply(spend_splitter, args=(all_spend,), axis=1)
-all_spend['3'] = None
-all_spend['12'] = None
-all_spend['Lifetime'] = None
+def calculate_all_spend(table):
+    _vehicle_spend = {}
+    for row in table.iterrows():
+        vehicle_id = row[1][0]
+        job_cost = row[1][2]
+        months = row[1][3]
+        if vehicle_id in _vehicle_spend.keys():
+            _vehicle_spend[vehicle_id]['spend'] += job_cost
+        else:
+            _vehicle_spend[vehicle_id] = {}
+            _vehicle_spend[vehicle_id]['spend'] = job_cost
+    return _vehicle_spend
+
+
+def calculate_three_month_spend(vehicle, lookup_table):
+    vehicle_id = vehicle['vehicle_id']
+    try:
+        return lookup_table[vehicle_id]['spend']
+    except:
+        return None
